463cb15a2aaff1ba77a6870be94d88b6a8cce9a0
13307425203 <qq_46054898@noreply.gitcode.com>
2025-05-28 00:45:03 +0800

    首页搭建

--- lib/handle/databaseHepler.dart
+++ lib/handle/databaseHepler.dart
@@ -4,20 +4,14 @@ import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 class DataBaseHelper {
   final String dataBaseName = "flash";
-
-
   Future<Database> create(String sql) async {
-
     databaseFactory = databaseFactoryFfi;
     sqfliteFfiInit();
     final database = openDatabase(
       join(await getDatabasesPath(), 'dome.db'),
       onCreate: (db, version) {
-
         return db.execute(sql);
       },
-      // Set the version. This executes the onCreate function and provides a
-      // path to perform database upgrades and downgrades.
       version: 1,
     );
     return database;
--- lib/ui/Home.dart
+++ lib/ui/Home.dart
@@ -6,6 +6,7 @@ import 'package:flashstart/utils/globle.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flashstart/main.dart';
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -18,6 +19,7 @@ class Home extends StatefulWidget {
 class HomeState extends State<Home> {
   bool isVisiableTips = true;
   String selectedPath = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,25 +28,67 @@ class HomeState extends State<Home> {
         child: Container(
           padding: EdgeInsets.all(10),
           decoration: BoxDecoration(color: Color.fromARGB(255, 23, 23, 23)),
-          alignment: Alignment.topLeft,
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Hearder(),
+              Container(
+                margin: EdgeInsets.only(top: 30, left: 30),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 400,
+                      height: 40,
+                      child: TextField(
+                        maxLines: 1,
+                        cursorColor: Colors.white,
+                        style: TextStyle(color: Colors.white, fontSize: 13),
+                        decoration: const InputDecoration(
+                          hintText: "输入搜索关键字",
+                          hintStyle: TextStyle(color: Colors.white),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.only(left: 10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      child: SizedBox(
+                        width: 100,
+                        height: 40,
+                        child: Center(
+                          child: Text(
+                            "搜索",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               Visibility(
                 visible: isVisiableTips,
                 child: Container(
                   alignment: Alignment.center,
-                  margin: EdgeInsets.only(top: 240),
+                  margin: EdgeInsets.only(top: 200),
                   child: GestureDetector(
-                    onTap: ()  {
-                      // 弹出文件选择框添加
-                      // startSelectedFile().then((value){
-                      //
-                      //   setState(()=>{
-                      //     selectedPath : value
-                      //   });
-                      // });
-                      // startSelectedFile();
+                    onTap: () {
+
                     },
                     child: Text(
                       "前往添加",
